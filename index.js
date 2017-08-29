7ea5e43e7d7f8602c0bfa7d1500bcd97c5f3093d
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-29 23:03:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-function nowServing() {
+function nowServing(deliLine) {
   if (katzDeliLine.length > 0 ) {
     var serving = katzDeliLine[0]
     katzDeliLine.shift(0)
